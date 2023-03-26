e4ed37f545b0565338c59e005dd01c52f3480abd
David Phillips <zantier.tasa@gmail.com>
2023-03-26 19:57:11 +0100

    Version 0.7

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       daft.ie hide
 // @namespace  http://daft-hide.daav.ee/
-// @version    0.6
+// @version    0.7
 // @description  hide properties on daft.ie
 // @match      *://*.daft.ie/*
 // @require    http://code.jquery.com/jquery-3.0.0.min.js
