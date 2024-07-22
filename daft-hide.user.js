b3ae720f449007b1ce79ad9301acb43a4674c05b
David Phillips <zantier.tasa@gmail.com>
2024-07-22 15:44:20 +0100

    Update data schema to version 2: Replace doShow with number value
    Also, add JSDoc types, to make errors easier to catch, and code easier
    to update.

--- daft-hide.user.js
+++ daft-hide.user.js
@@ -9,11 +9,28 @@
 // @grant      GM_setValue
 // ==/UserScript==
 
+/**
+ * @typedef StoreData
+ * @type {object}
+ * @property {2} version - Whether the house is good or bad
+ * @property {Object.<string, House>} urls - description of the house
+ */
+
+/**
+ * @typedef House
+ * @type {object}
+ * @property {number} value - Whether the house is good or bad
+ * @property {string} desc - description of the house
+ */
+
 (function() {
   'use strict';
 
   let housesStoreIndex = 'daft_hide';
   // Indexed by URL.
+  /**
+   * @type {Object.<string, House>}
+   */
   let houses = {};
   let mutationObserver = new MutationObserver(function(changes) {
     onLoadPage();
@@ -27,7 +44,7 @@
     // it performs a single mutation event on the react root element
     mutationObserver.observe(document.body, { childList:true, subtree:false });
 
-    houses = getUrlInfo();
+    houses = getStoreData().urls;
 
     // Single property, or property search
     // Logging this first helps to see if the script is completely broken
@@ -49,6 +66,7 @@
     log(`pageType: ${pageType}`);
 
     if (pageType === 'search') {
+      if (!resultsElement) throw Error('resultsElement cannot be null');
       mutationObserver.observe(resultsElement, { childList:true, subtree:false });
       for (let box of resultsElement.children) {
         initializeBox(box);
@@ -60,18 +78,25 @@
     }
 
     if (pageType === 'map') {
+      if (!mapSearchWrapper) throw Error('mapSearchWrapper cannot be null');
       // After clicking a property on the map, then "Back", the wrapper loads before the carousel
       mutationObserver.observe(mapSearchWrapper, { childList: true, subtree: false });
-      if (!elementExists({ mapSearchCarousel })) {
+      if (mapSearchCarousel) {
+
+      }
+      if (!mapSearchCarousel) {
+        elementExists({ mapSearchCarousel })
         return;
       }
       mutationObserver.observe(mapSearchCarousel, { childList: true, subtree: false });
-      if (!elementExists({ mapSearch })) {
+      if (!mapSearch) {
+        elementExists({ mapSearch })
         return;
       }
       mutationObserver.observe(mapSearch, { childList: true, subtree: false });
       let mapPricesParent = mapSearch.children[0];
-      if (!elementExists({ mapPricesParent })) {
+      if (!mapPricesParent) {
+        elementExists({ mapPricesParent });
         return;
       }
       mutationObserver.observe(mapPricesParent, { childList: true, subtree: false });
@@ -85,10 +110,11 @@
         if (data && data.desc) {
           color = '#00a0f0';
         }
-        if (data && !data.doShow) {
+        if (data && data.value === -1) {
           color = 'grey';
         }
-        mapPrices[i+1].children[0].style.backgroundColor = color;
+        /** @type {HTMLElement} */
+        (mapPrices[i+1].children[0]).style.backgroundColor = color;
       }
 
       let popupBox = mapSearch.querySelector('*[data-testid^="pop-up"]')
@@ -98,41 +124,49 @@
     }
   }
 
-  // Get the list of URLs to ignore.
-  function getUrlInfo() {
-    let result = {};
+  /**
+   * Get the list of URLs to ignore.
+   * @returns {StoreData}
+   */
+  function getStoreData() {
     let catValues = GM_getValue(housesStoreIndex, '');
-    if (catValues) {
-      result = JSON.parse(catValues);
+    if (!catValues) {
+      return { version: 2, urls: {} };
     }
 
+    let data = JSON.parse(catValues);
+    let result = upgradeVersion2(data);
     return result;
   }
 
-  // Initialize a box containing a house.
-  function initializeBox(box, isMapPopup) {
+  /**
+   * Initialize a box containing a house.
+   * @param {Element} box
+   * @param {boolean} isMapPopup
+   * @returns {House | undefined}
+   */
+  function initializeBox(box, isMapPopup=false) {
     // Each box has an image (either to the left, or large image above)
     // then a line for the price, and a line for the address
 
-    let exists = true;
     // address on search page, sub-line-2-info on map popup
     let titleBox = box.querySelector('*[data-testid="address"],*[data-testid="sub-line-2-info"]');
-    exists = exists && elementExists({ titleBox });
+    elementExists({ titleBox });
     let urlBox = box.querySelector(':scope > a');
-    exists = exists && elementExists({ urlBox });
+    elementExists({ urlBox });
 
     // price on search page, sub-title on map popup
     let priceBox = box.querySelector('*[data-testid="price"],*[data-testid="sub-title"]');
-    exists = exists && elementExists({ priceBox });
+    elementExists({ priceBox });
 
-    if (!exists) {
+    if (!titleBox || !urlBox || !priceBox) {
       return;
     }
 
     let floorAreaBox = box.querySelector('*[data-testid="floor-area"]');
-    let floorArea = floorAreaBox?.textContent;
-    let price = priceBox.textContent;
-    let url = urlBox.getAttribute('href');
+    let floorArea = floorAreaBox?.textContent ?? '';
+    let price = priceBox.textContent ?? '';
+    let url = urlBox.getAttribute('href') ?? '';
 
     initializeControls(url, titleBox, price, floorArea, box, isMapPopup);
     let house = houses[url];
@@ -141,29 +175,38 @@
 
   // Show stuff on property page.
   function updatePropertyPage() {
-    let exists = true;
-    let titleBox = document.querySelector('*[data-testid="address"],*[data-testid="alt-title"]')
-    exists = exists && elementExists({ titleBox });
+    let titleBox = document.querySelector('*[data-testid="address"],*[data-testid="alt-title"]');
+    elementExists({ titleBox });
 
     let priceBox = document.querySelector('*[data-testid="price"]');
-    exists = exists && elementExists({ priceBox });
+    elementExists({ priceBox });
 
-    if (!exists) {
+    if (!titleBox || !priceBox) {
       return;
     }
 
     let floorAreaBox = document.querySelector('*[data-testid="floor-area"]');
-    let floorArea = floorAreaBox?.textContent;
-    let price = priceBox.textContent;
+    let floorArea = floorAreaBox?.textContent ?? '';
+    let price = priceBox.textContent ?? '';
     let url = window.location.pathname;
 
     initializeControls(url, titleBox, price, floorArea, undefined);
   }
 
-  // This will be used both in search results and property page
-  function initializeControls(url, titleBox, priceText, floorAreaText, hideBox, isMapPopup) {
+  /**
+   * This will be used both in search results and property page
+   * @param {string} url
+   * @param {Element} titleBox
+   * @param {string} priceText
+   * @param {string} floorAreaText
+   * @param {Element | undefined} hideBox
+   * @param {boolean} isMapPopup
+   * @returns
+   */
+  function initializeControls(url, titleBox, priceText, floorAreaText, hideBox, isMapPopup=false) {
+    let titleParent = assertParent(titleBox);
     // When filtering search results, initialize can run multiple times on the same property
-    let alreadyInitialized = titleBox.parentElement.querySelector('.checkbox_ignore');
+    let alreadyInitialized = titleParent.querySelector('.checkbox_ignore');
     if (alreadyInitialized) {
       return;
     }
@@ -174,27 +217,30 @@
     let checkedText = 'checked="true"';
     if (house) {
       desc = house.desc;
-      if (!house.doShow) {
+      if (house.value === -1) {
         showAndHide(hideBox, false);
         checkedText = '';
       }
     }
 
-    if (isMapPopup) {
+    if (isMapPopup && hideBox) {
       // On map popup, wrap controls to new lines
-      titleBox.parentElement.style.flexWrap = 'wrap';
+      titleParent.style.flexWrap = 'wrap';
+      /** @type {HTMLElement | null} */
       let urlBox = hideBox.querySelector(':scope > a');
-      // Make the box wider, to fit the textbox
-      urlBox.style.display = 'inline-flex';
-      urlBox.style.backgroundColor = 'rgba(255,255,255,0.7)';
-      urlBox.parentElement.parentElement.parentElement.style.backgroundColor = 'transparent';
+      if (urlBox) {
+        // Make the box wider, to fit the textbox
+        urlBox.style.display = 'inline-flex';
+        urlBox.style.backgroundColor = 'rgba(255,255,255,0.7)';
+        assertParent(assertParent(assertParent(urlBox))).style.backgroundColor = 'transparent';
+      }
     }
-    let checkboxContainer = createElementFromHTML('<div class="checkbox-container"></div>');
-    let descContainer = createElementFromHTML('<div class="desc-container"></div>');
+    let checkboxContainer = /** @type {ChildNode} */ (createElementFromHTML('<div class="checkbox-container"></div>'));
+    let descContainer = /** @type {ChildNode} */ (createElementFromHTML('<div class="desc-container"></div>'));
     insertBefore(checkboxContainer, titleBox);
     insertAfter(descContainer, titleBox);
-    let checkbox = createElementFromHTML('<input class="checkbox_ignore" type="checkbox" ' + checkedText + ' />');
-    let descDiv = createElementFromHTML('<textarea class="desc" style="resize:none; width:400px">' + desc + '</textarea>');
+    let checkbox = /** @type {HTMLInputElement} */ (createElementFromHTML('<input class="checkbox_ignore" type="checkbox" ' + checkedText + ' />'));
+    let descDiv = /** @type {HTMLTextAreaElement} */ (createElementFromHTML('<textarea class="desc" style="resize:none; width:400px">' + desc + '</textarea>'));
 
     let price = toNumber(priceText);
     let floorArea = toNumber(floorAreaText);
@@ -228,11 +274,20 @@
     });
   }
 
+  /**
+   * @param {HTMLElement} element
+   */
   function auto_grow(element) {
     element.style.height = '5px';
     element.style.height = (element.scrollHeight)+'px';
   }
 
+  /**
+   *
+   * @param {Element | undefined} box
+   * @param {boolean} doShow
+   * @returns
+   */
   function showAndHide(box, doShow) {
     if (!box) {
       return;
@@ -254,43 +309,57 @@
       for (let selector of selectors) {
         let elements = box.querySelectorAll(selector);
         for (let element of elements) {
-          element.style.display = '';
+          /** @type {HTMLElement} */
+          (element).style.display = '';
         }
       }
     } else {
       for (let selector of selectors) {
         let elements = box.querySelectorAll(selector);
         for (let element of elements) {
-          element.style.display = 'none';
+          /** @type {HTMLElement} */
+          (element).style.display = 'none';
         }
       }
     }
   }
 
-  function submitChange(url, checkbox, descDiv, box) {
+  /**
+   * @param {string} url
+   * @param {HTMLInputElement} checkbox
+   * @param {HTMLTextAreaElement} descElement
+   * @param {Element | undefined} box
+   */
+  function submitChange(url, checkbox, descElement, box) {
     let doShow = checkbox.checked;
-    let desc = descDiv.value;
+    let desc = descElement.value;
 
     showAndHide(box, doShow);
 
-    let house = houses[url];
     let inList = !doShow || desc;
 
     if (inList) {
       houses[url] = {
-        doShow: doShow,
+        value: doShow ? 1 : -1,
         desc: desc,
       };
     } else {
       delete houses[url];
     }
 
-    //console.log(houses);
-    GM_setValue(housesStoreIndex, JSON.stringify(houses));
+    /** @type {StoreData} */
+    let data = {
+      version: 2,
+      urls: houses,
+    };
+    GM_setValue(housesStoreIndex, JSON.stringify(data));
   }
 
-  // Convert string to number, stripping out any non-digit characters. If there are no
-  // digits, return NaN.
+  /**
+   * Convert string to number, stripping out any non-digit characters. If there are no
+   * digits, return NaN.
+   * @param {string} str
+   */
   function toNumber(str) {
     let digits = '';
     if (str) {
@@ -304,8 +373,12 @@
     return parseInt(digits);
   }
 
-  // Get (and log) if the element exists
-  // elementObject: { elementName: element }
+  /**
+   * Get (and log) if the element exists
+   * elementObject: { elementName: element }
+   * @param {Object.<string, Element?>} elementObject
+   * @returns
+   */
   function elementExists(elementObject) {
     let elementName = Object.keys(elementObject)[0];
     let element = Object.values(elementObject)[0];
@@ -316,24 +389,83 @@
     return exists;
   }
 
-  // Create a DOM element from the given HTML string
+  /**
+   * Create a DOM element from the given HTML string
+   * @param {string} htmlString
+   */
   function createElementFromHTML(htmlString) {
     var div = document.createElement('div');
     div.innerHTML = htmlString.trim();
-    return div.firstChild;
+    return /** @type {ChildNode} */ (div.firstChild);
   }
 
+  /**
+   * @param {Node} newNode
+   * @param {Node} referenceNode
+   */
   function insertBefore(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode);
+    if (referenceNode.parentNode) {
+      referenceNode.parentNode.insertBefore(newNode, referenceNode);
+    }
   }
+  /**
+   * @param {Node} newNode
+   * @param {Node} referenceNode
+   */
   function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    if (referenceNode.parentNode) {
+      referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
   }
 
+  /**
+   * @param {string} msg
+   */
   function log(msg) {
     console.log(`daft_hide: ${msg}`);
   }
+  /**
+   * @param {string} msg
+   */
   function logError(msg) {
     console.error(`daft_hide: ${msg}`);
+  }
+
+  /**
+   * @param {*} oldData
+   * @returns {StoreData}
+   */
+  function upgradeVersion2(oldData) {
+    if ('version' in oldData) {
+      return oldData;
+    }
+
+    /** @type {StoreData} */
+    let newData = {
+      version: 2,
+      urls: {},
+    };
+
+    for (let url of Object.keys(oldData)) {
+      newData.urls[url] = {
+        value: oldData[url].doShow ? 1 : -1,
+        desc: oldData[url].desc,
+      };
+    }
+
+    return newData;
+  }
+
+  /**
+   * Get parent element, asserting that it exists
+   * @param {Element} element
+   * @returns {HTMLElement}
+   */
+  function assertParent(element) {
+    if (!element.parentElement) {
+      throw Error('Failed to get parent element');
+    }
+
+    return element.parentElement;
   }
 })();
